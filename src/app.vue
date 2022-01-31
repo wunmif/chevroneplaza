07391a62e0b5e797fb6b2489de3e3c6a4b5ec448
Wunmi Dabiri <oluwafumbidabiri@gmail.com>
2022-01-31 23:38:20 +0100

    first commit

---
+++ src/app.vue
@@ -0,0 +1,47 @@
+<template >
+    <div id="app" class="bg-white  overflow-hidden cf w-100 vh-100 flex flex-column">
+        <navbar />
+        
+        <router-view class="h-100 w-100 overflow-scroll"></router-view>
+    </div>
+</template>
+
+<!-- App specific objects  -->
+
+
+<script type="text/javascript">
+    module.exports = {
+        name: "app",
+    }
+</script>
+
+<script type="text/javascript">
+    import navbar from "@/components/reuseables/navbar";
+   
+   
+    export default {
+        components: {
+            navbar,
+            
+        },
+        created() {
+        },
+    }
+</script>
+
+<style lang="scss">
+    // @import "assets/scss/tachyons.css";
+</style>
+
+
+<style media="screen">
+    @import "assets/css/fontawesomepro.css";
+    @import "assets/css/tachyons-sass/tachyons.scss";
+    @import "assets/css/style.css";
+
+</style>
+
+
+
+
+
---
+++ src/components/home.vue
@@ -0,0 +1,52 @@
+<template >
+    <section  class=" w-100 pt6-l pt3 ph5-l ph3 overflow-scroll cf " >
+        <div class="dt pv5 w-100 ">
+            <div class="fl fr-l w-50-l w-100">
+               <img src="@/assets/img/finance.svg" class=" h-75-l w-100 h5">
+            </div>
+
+            <div class="fl  w-50-l w-100">
+                <p class="f1">
+                    Be where the market is
+                </p>
+                <p class="mt0 f4">
+                    Anyone, anywhere, can launch an online store while working from home.
+                </p>
+                <span class="ba pa3 br1 b--silver center bg-white">
+                    <input type="text" placeholder="enter your store name" class="center bg-transparent near-black tr-ns tc bn">
+                    <span class="center pr3 near-black tl bn dn dib-ns"></span>
+                </span>
+                <a target="_blank">
+                    <span class="pa3 br1 bg-near-black ba b--dark-gray washed-yellow fw5">
+                        Launch a store
+                    </span>
+                </a>
+
+            </div>
+
+
+
+        </div>
+       
+   
+        
+        
+    </section>
+</template>
+
+<script>
+    
+   
+    import navbar from "@/components/reuseables/navbar"
+
+
+    export default {
+        name: 'home',
+        components: {navbar,},
+        data(){return{
+
+        }},
+    }
+
+    
+</script>
